c66c8585b5daabac2946559573d250fba90fe48c
cristian mtr <cristi.mtr@gmail.com>
2015-10-18 17:55:27 +0200

    taskEndpoint proven endpoints; GET and POST; replying with mock data;

--- src/main/java/tracker_java/Controllers/taskEndpointHandler.java
+++ src/main/java/tracker_java/Controllers/taskEndpointHandler.java
@@ -9,29 +9,56 @@ import tracker_java.Models.Item;
 import tracker_java.Utilities.JsonResponseHandler;
 import tracker_java.Utilities.errorHandler;
 
+import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
 
 /**
  * first checks what the URI is;
  * if /task/<id>
- *     just returns the json representation of the task of that id
- *  if /task/<id>/comments
- *      return json of comments
- *  if /task/<id>/history
- *      return json of history
+ * just returns the json representation of the task of that id
+ * if /task/<id>/comments
+ * return json of comments
+ * if /task/<id>/history
+ * return json of history
  */
 public class taskEndpointHandler implements HttpHandler {
 
-    public void handleComments(HttpExchange httpExchange, int taskId) {
-
+    private void handlePostComments(HttpExchange httpExchange, int taskId) {
+        String res = "post comment";
+        JsonResponseHandler.INSTANCE.replyWithJsonFromObject(httpExchange, res);
     }
 
-    public void handleHistory(HttpExchange httpExchange, int taskId) {
-
+    private void handlePostHistory(HttpExchange httpExchange, int taskId) {
+        String res = "post history";
+        JsonResponseHandler.INSTANCE.replyWithJsonFromObject(httpExchange, res);
     }
 
-    public void handleTask(HttpExchange httpExchange, int taskId) {
+    private void handleUpdateTask(HttpExchange httpExchange, int taskId) {
+        String res = "update existing";
+        JsonResponseHandler.INSTANCE.replyWithJsonFromObject(httpExchange, res);
+    }
+
+    private void handlePostNewTask(HttpExchange httpExchange) {
+        String res = "post new";
+        JsonResponseHandler.INSTANCE.replyWithJsonFromObject(httpExchange, res);
+    }
+
+    public void handleGetComments(HttpExchange httpExchange, int taskId) {
+        HashMap response = new HashMap();
+        response.put("code", "200");
+        response.put("data", String.format("task %d comments", taskId));
+        JsonResponseHandler.INSTANCE.replyWithJsonFromObject(httpExchange, response);
+    }
+
+    public void handleGetHistory(HttpExchange httpExchange, int taskId) {
+        HashMap response = new HashMap();
+        response.put("code", "200");
+        response.put("data", String.format("task %d history", taskId));
+        JsonResponseHandler.INSTANCE.replyWithJsonFromObject(httpExchange, response);
+    }
+
+    public void handleGetTask(HttpExchange httpExchange, int taskId) {
         Session s = HibernateUtil.getSessionFactory().openSession();
         s.beginTransaction();
         Query query = s.createQuery("from Item where itemId = :taskId").setParameter("taskId", taskId);
@@ -39,9 +66,8 @@ public class taskEndpointHandler implements HttpHandler {
         if (list.size() > 1) {
             System.out.format("ERROR: 500: MORE THAN ONE TASK WITH SAME ID: %d%n", taskId);
             errorHandler.INSTANCE.returnServerError(httpExchange);
-        }
-        else {
-            Item theTask = (Item)list.get(0);
+        } else {
+            Item theTask = (Item) list.get(0);
             HashMap response = new HashMap();
             response.put("code", "200");
             response.put("data", theTask);
@@ -49,38 +75,86 @@ public class taskEndpointHandler implements HttpHandler {
         }
     }
 
-    public void handle(HttpExchange httpExchange){
-        System.out.println("request at /task");
+    public void handleGet(HttpExchange httpExchange) {
         String[] fullURISplit = httpExchange.getRequestURI().toString().split("/");
-        int taskId = 0;
-
-        try {
-            taskId = Integer.parseInt(fullURISplit[2]);
-        }
-        catch (NumberFormatException e) {
-            System.out.format("task passed via URL was not appropriate int: %s", fullURISplit[2]);
+        String matchedCase = UrlMatcher.INSTANCE.match(httpExchange.getRequestURI().toString());
+        if (matchedCase == null) {
+            System.out.format("unknown request path: %s%n", httpExchange.getRequestURI().toString());
             errorHandler.INSTANCE.returnBadRequest(httpExchange);
+        } else {
+            int taskId = Integer.parseInt(fullURISplit[2]);
+            System.out.format("taskId = %d%n", taskId);
+            switch (matchedCase) {
+                case "/task/id/comments":
+                    // handle comments
+                    System.out.println("// handle comments");
+                    handleGetComments(httpExchange, taskId);
+                    break;
+                case "/task/id/history":
+                    // handle history
+                    System.out.println("// handle history");
+                    handleGetHistory(httpExchange, taskId);
+                    break;
+                case "/task/id":
+                    // handle task itself
+                    System.out.println("// handle task itself");
+                    handleGetTask(httpExchange, taskId);
+                    break;
+            }
+        }
+    }
+
+    /*
+    POST /task - new task with auto ID generation
+    POST /task/<id> - update task with ID <id> with new info
+    POST /task/<id>/comment - post comment to task with id
+    POST /task/<id>/history - post status change to task with id
+     */
+    public void handlePost(HttpExchange httpExchange) {
+        String[] fullURISplit = httpExchange.getRequestURI().toString().split("/");
+        String matchedCase = UrlMatcher.INSTANCE.match(httpExchange.getRequestURI().toString());
+        if (matchedCase == null) {
+            System.out.format("unknown request path: %s%n", httpExchange.getRequestURI().toString());
+            errorHandler.INSTANCE.returnBadRequest(httpExchange);
+        } else {
+            int taskId;
+            switch (matchedCase) {
+                case "/task/id/comments":
+                    // handle comments
+                    taskId = Integer.parseInt(fullURISplit[2]);
+                    System.out.format("taskId = %d%n", taskId);
+                    System.out.println("// handle comments");
+                    handlePostComments(httpExchange, taskId);
+                    break;
+                case "/task/id/history":
+                    // handle history
+                    taskId = Integer.parseInt(fullURISplit[2]);
+                    System.out.format("taskId = %d%n", taskId);
+                    System.out.println("// handle history");
+                    handlePostHistory(httpExchange, taskId);
+                    break;
+                case "/task/id":
+                    // handle task itself
+                    taskId = Integer.parseInt(fullURISplit[2]);
+                    System.out.format("taskId = %d%n", taskId);
+                    System.out.println("// handle update existing task");
+                    handleUpdateTask(httpExchange, taskId);
+                    break;
+                case "/task":
+                    System.out.format("// handle new task%n");
+                    handlePostNewTask(httpExchange);
+                    break;
+            }
         }
 
-        System.out.format("taskId = %d%n", taskId);
-        switch (fullURISplit[(fullURISplit).length - 1]) {
-            case "comments":
-                // handle comments
-                System.out.println("// handle comments");
-                handleComments(httpExchange, taskId);
-                break;
-            case "history":
-                // handle history
-                System.out.println("// handle history");
-                handleHistory(httpExchange, taskId);
-                break;
-            default:
-                // handle task itself
-                System.out.println("// handle task itself");
-                handleTask(httpExchange, taskId);
-                break;
+    }
+
+    public void handle(HttpExchange httpExchange) {
+        System.out.format("%s request at /task%n", httpExchange.getRequestMethod());
+        if (httpExchange.getRequestMethod().equals("GET")) {
+            this.handleGet(httpExchange);
+        } else if (httpExchange.getRequestMethod().equals("POST")) {
+            this.handlePost(httpExchange);
         }
-
-
     }
 }
