59b6f997d982aa920017f33e8cc8164c8a901f74
cristimtr <cristi.mtr@gmail.com>
2015-11-10 14:21:38 +0100

    cleanup: unused imports in userHandler

--- src/main/java/tracker_java/Controllers/userHandler.java
+++ src/main/java/tracker_java/Controllers/userHandler.java
@@ -1,19 +1,11 @@
 package tracker_java.Controllers;
 
-import tracker_java.Models.FormField;
 import tracker_java.Models.FormGenerator;
 import tracker_java.Models.MemberEntity;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.beans.BeanInfo;
-import java.beans.IntrospectionException;
-import java.beans.Introspector;
-import java.beans.PropertyDescriptor;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 
 /**
