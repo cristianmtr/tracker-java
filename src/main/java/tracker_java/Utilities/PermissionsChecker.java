d21939f22f678d47b4853ef10a591fce27dffd34
cristimtr <cristi.mtr@gmail.com>
2015-11-12 14:23:09 +0100

    permissions checker gets username from token; some error/exception handling

--- src/main/java/tracker_java/Utilities/PermissionsChecker.java
+++ src/main/java/tracker_java/Utilities/PermissionsChecker.java
@@ -6,11 +6,13 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.glassfish.jersey.server.ContainerRequest;
+import redis.clients.jedis.Jedis;
 
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import java.util.IllegalFormatException;
+import java.util.List;
 import java.util.regex.Pattern;
 
 /**
@@ -18,23 +20,64 @@ import java.util.regex.Pattern;
  */
 
 @Aspect
-public class PermissionsChecker {
+public final class PermissionsChecker {
 
     @Around("execution(* *.*(..)) && @annotation(PermissionRequirements) ")
-    public Response checkPermissionsIntercept(ProceedingJoinPoint joinPoint) throws Exception {
-
+    public Object checkPermissionsIntercept(ProceedingJoinPoint joinPoint) throws Exception {
         System.out.println("checking permissions");
         ContainerRequest request = (ContainerRequest) joinPoint.getArgs()[0];
-        String[] userNamePassword = AuthenticationHandler.INSTANCE.decode(request.getRequestHeader("authorization").get(0));
-        System.out.println("user : " + userNamePassword[0]);
-        String path = request.getPath(true);
-        String[] splitPath = path.split(Pattern.quote("/"));
-
-//        if ( true ) {
-//            throw new UnauthorizedException();
-//        }
+        // token exist check
+        // get user of token
+        // get user permission on resource
+        // check is permission enough
+        if (this.userOfTokenHasPermission(request)) {
+            try {
+                return joinPoint.proceed();
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        };
         return Response.status(401).build();
+    }
 
+    private boolean userOfTokenHasPermission(ContainerRequest request) {
+        String token = getToken(request);
+        Integer memberId = getUserIdFromToken(token);
+        String path = request.getPath(true);
+        System.out.format("request at %s from userId %s%n", path, memberId);
+
+        return false;
+    }
+
+    private Integer getUserIdFromToken(String token) {
+        try {
+            Jedis redis = JedisPoolInstance.pool.getResource();
+            return Integer.parseInt(redis.get(token));
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String getToken(ContainerRequest request) {
+        try {
+            String bareHeader = request.getRequestHeader("authorization").get(0);
+            // should be ["Bearer", $token]
+            String[] typeAndToken = bareHeader.split(Pattern.quote(" "));
+            if (typeAndToken[0].equals("Bearer") && typeAndToken[1].length() > 0) {
+                return typeAndToken[1];
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        // otherwise Bad Request
+        throw new WebApplicationException(Response.status(400).build());
+    }
+
+    private String decodeToken(ContainerRequest request) {
+        return null;
     }
 
 }
